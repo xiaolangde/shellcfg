20548505b16ab0884e4a6fabb8a2dd716cc49688
xiaolangde <xiaolangde@gmail.com>
2013-08-25 21:38:47 +0800

    first commit

---
+++ init.bash.sh
@@ -0,0 +1,9 @@
+## define the term
+export TERM=xterm-256color
+
+##use emacs
+alias emacsrun='emacs --daemon'
+alias e='emacsclient -t'
+alias ec='emacsclient -c'
+#alias vim='emacsclient -t'
+#alias vi='emacsclient -t'
---
+++ init.profile.sh
@@ -0,0 +1,2 @@
+#switch  the key CONTROL_L and caps_lock
+. ~/z.shell.d/remap-key.sh
